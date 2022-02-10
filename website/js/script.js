9aa41992621044dd7b744a269328bd39d63c3f11
Keegan Lenz <Keegan.lenz@student.cart.org>
2022-02-09 16:46:29 -0800

    images from youtube thumbnail api

--- website/js/script.js
+++ website/js/script.js
@@ -1,3 +1,3 @@
 function startGame() {
-    document.getElementById("page-wrapper").innerHTML = "<div class='container1'><div class='img1'><img src='imgs/test-img1.PNG' alt='first-img'></div></div> <div class='container2'><div class='img2'><img src='imgs/test-img2.PNG' alt='second-img'></div></div>"
+    document.getElementById("page-wrapper").innerHTML = "<div class='container1'><div class='img1'><img src='imgs/test-img1.jpg' alt='first-img'></div></div> <div class='container2'><div class='img2'><img src='imgs/test-img2.jpg' alt='second-img'></div></div>"
 }
