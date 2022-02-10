e45c5c9bdc6fd64f2ca095edb0c7c15cc571e720
Keegan Lenz <Keegan.lenz@student.cart.org>
2022-02-09 16:11:02 -0800

    basic layout complete

---
+++ website/js/script.js
@@ -0,0 +1,3 @@
+function startGame() {
+    document.getElementById("page-wrapper").innerHTML = "<div class='container1'><div class='img1'><img src='imgs/test-img1.PNG' alt='first-img'></div></div> <div class='container2'><div class='img2'><img src='imgs/test-img2.PNG' alt='second-img'></div></div>"
+}
