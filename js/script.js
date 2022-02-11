8477086f876b8f1052504838d849b20db73fea80
Keegan Lenz <Keegan.lenz@student.cart.org>
2022-02-10 18:25:10 -0800

    checks if correct guess was made

--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
+vid_views1 = "10,166,751,485";
+view_num1 = parseInt(vid_views1.replace(/,/g, ''));
+
+vid_title1 = "Baby Shark Dance | #babyshark Most Viewed Video | Animal Songs | PINKFONG Songs for Children";
+vid_thumbnail1 = "test-img1.jpg";
+
+vid_views2 = "7,735,183,650";
+view_num2 = parseInt(vid_views2.replace(/,/g, ''));
+
+vid_title2 = "Luis Fonsi - Despacito ft. Daddy Yankee";
+vid_thumbnail2 = "test-img2.jpg";
+
 function startGame() {
-    document.getElementById("page-wrapper").innerHTML = "<img class='background-img1' src='imgs/test-img1.jpg' alt='first-background-img'><div class='container1'><div class='img1'><img src='imgs/test-img1.jpg' alt='first-img'></div></div> <img class='background-img2' src='imgs/test-img2.jpg' alt='second-background-img'><div class='container2'><div class='img2'><img src='imgs/test-img2.jpg' alt='second-img'></div></div>"
+    document.getElementById("page-wrapper").innerHTML = ""
+}
+
+function lower() {
+    document.getElementById("output").innerHTML = "<h2>" + vid_views2 + "</h2>"
+    if (view_num1 > view_num2) {
+        document.getElementById("output-cirlce").innerHTML = "&#10004;"
+        document.getElementById("output-cirlce").style.backgroundColor = "green;"
+    }else{
+        outputCirlce.innerHTML = "&#10006;"
+        outputCirlce.style.backgroundColor = "red;"
+    }
+}
+
+function higher() {
+    document.getElementById("output").innerHTML = "<h2>" + vid_views2 + "</h2>"
+    if (view_num2 > view_num1) {
+        document.getElementById("output-cirlce").innerHTML = "&#10004;"
+        document.getElementById("output-circle").style.backgroundColor = "green;"
+    }else{
+        document.getElementById("output-cirlce").innerHTML = "&#10006;"
+        document.getElementById("output-circle").style.backgroundColor = "red;"
+    }
 }
